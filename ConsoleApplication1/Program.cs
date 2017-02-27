afa3d6498eef3718b3b3ffb10e2b14e45177a9bc
eng250 <eng250@nyu.edu>
2017-02-27 08:25:49 -0600

    Wrote a program to determine gas mileage, based on a user's inputed distance traveled and fuel usage in gallons.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double miles;
+            double gallons;
+            double mpg;
+            string input;
+
+            Console.Write("How many miles have you driven?");
+            input = Console.ReadLine();
+            miles = double.Parse(input);
+
+            Console.Write("How many gallons of gas have you used?");
+            input = Console.ReadLine();
+            gallons = double.Parse(input);
+
+            mpg = miles / gallons;
+            Console.Write("You are getting " + mpg + " mpg");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
