b6d01838ce51a1569421e57dc41c4bfa6b2f05dc
Joshua Krigelman <krigelmanj@gmail.com>
2017-03-06 17:25:58 -0600

    duh

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            {
+                foreach (KeyValuePair<string, string> item in definitions)
+                {
+                    Console.WriteLine(definitions.Key + ") " + definitions.Value );
+                    
+    
+
+            }
+            }
+        }
+    }
+}
+
+        }
+    }
+}
---
+++ CountingCharacters/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String testString;
+            testString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+            Dictionary<char, int> keyCount =
+                new Dictionary<char, int>();
+
+            foreach (char c in testString)
+            {
+                string charCheck = c.ToString(); 
+                if (!keyCount.ContainsKey(c))
+                {
+                    keyCount.Add(c, 0);
+                    keyCount[c] += 1; 
+                }
+                else
+                {
+                    keyCount[c] += 1;
+                }
+            }
+
+            //StringBuilder charList = new StringBuilder();
+
+            foreach (KeyValuePair<char, int> charCount in keyCount)
+            {
+                string charHolder = charCount.Value.ToString();
+                Console.WriteLine(charCount.Key + " : " + charHolder);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
